746b6a63523b975553ea07426e639dfeb2c6336f
Colin <cfinn68916@gmail.com>
2024-01-14 23:10:00 -0600

    Split release script into two parts
    Signed-off-by: Colin <cfinn68916@gmail.com>

---
+++ export_files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kibot
+mv 3D/Encoder_Breakout-3D_bottom.png 3D/bottom.png
+mv 3D/Encoder_Breakout-3D.step 3D/board.step
+mv 3D/Encoder_Breakout-3D_top30deg.png 3D/30deg.png
+mv 3D/Encoder_Breakout-3D_top.png 3D/top.png
+mv Schematic/Encoder_Breakout-schematic.pdf Schematic/sch.pdf
+mv Manufacturers/Encoder_Breakout-_JLCPCB_compress.zip Manufacturers/JLC.zip
